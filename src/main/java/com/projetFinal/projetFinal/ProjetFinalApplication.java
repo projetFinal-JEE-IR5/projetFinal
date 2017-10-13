dacbaa2254b4977a6da2284e9c1d3311e41941ec
bsabaron.ir2018 <bsabaron.ir2018@E007-14.esaip.local>
2017-10-13 10:16:50 +0200

    test

---
+++ src/main/java/com/projetFinal/projetFinal/ProjetFinalApplication.java
@@ -0,0 +1,12 @@
+package com.projetFinal.projetFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFinalApplication.class, args);
+	}
+}
---
+++ src/test/java/com/projetFinal/projetFinal/ProjetFinalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.projetFinal.projetFinal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetFinalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
