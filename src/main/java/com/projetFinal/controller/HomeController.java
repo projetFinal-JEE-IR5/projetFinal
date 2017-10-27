532edc66e73f736edbb01a12442243e53ad31874
bsabaron.ir2018 <bsabaron.ir2018@E007-10.esaip.local>
2017-10-27 11:33:42 +0200

    homeController

--- src/main/java/com/projetFinal/controller/HomeController.java
+++ src/main/java/com/projetFinal/controller/HomeController.java
@@ -2,21 +2,23 @@ package com.projetFinal.controller;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
-	// inject via application.properties
-	@Value("${welcome.message:test}")
-	private String message = "Hello World";
-
-	@RequestMapping("/")
+	@GetMapping("/welcome")
 	public String welcome(Map<String, Object> model) {
-		model.put("message", this.message);
+		model.put("message", "test");
 		return "welcome";
+	}
+	
+	@PostMapping("/welcome")
+	public void test(@RequestParam Map<String, String> formValues) {
+		System.out.println(formValues.get("login")+formValues.get("password"));
 	}
 
 }
