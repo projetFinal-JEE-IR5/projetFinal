198d5360e0fb03e3ff29cc856b3da8a8a88f7d6d
arichard.ir2018 <arichard.ir2018@E007-11.esaip.local>
2017-10-30 17:35:02 +0100

    version propre

--- src/main/java/com/projetFinal/ProjetFinalApplication.java
+++ src/main/java/com/projetFinal/ProjetFinalApplication.java
@@ -3,8 +3,6 @@ package com.projetFinal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.projetFinal.model.metier.Personne;
-
 @SpringBootApplication
 public class ProjetFinalApplication {
 
